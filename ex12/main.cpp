45f1a8daaf9b1994b70b42a2795d52eeb2a87428
Vitaliy Shavnin <crowf4i@gmail.com>
2023-02-13 00:43:03 +0300

    ex12 done

---
+++ ex12/main.cpp
@@ -0,0 +1,37 @@
+#include "../Matrix.hpp"
+#include "../Vector.hpp"
+
+using namespace ft;
+
+void	test_in_task()
+{
+	std::cout << "------ Inverse in task ------" << std::endl;
+
+	ft::Matrix<double> u = {{1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}};
+	std::cout << "{{1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}}" << std::endl;
+	std::cout << u.inverse() << std::endl;
+
+	u = {{2., 0., 0.}, {0., 2., 0.}, {0., 0., 2.}};
+	std::cout << "{{2., 0., 0.}, {0., 2., 0.}, {0., 0., 2.}}" << std::endl;
+	std::cout << u.inverse() << std::endl;
+
+	u = {{8., 5., -2.}, {4., 7., 20.}, {7., 6., 1.}};
+	std::cout << "{{8., 5., -2.}, {4., 7., 20.}, {7., 6., 1.}}" << std::endl;
+	std::cout << u.inverse() << std::endl;
+}
+
+void	test_additional_any_matrix()
+{
+
+}
+
+void	test_additional_error()
+{
+
+}
+
+int	main()
+{
+	test_in_task();
+
+}
